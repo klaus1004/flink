e8ac7f2887904b93b2df8de065e7d0aa0f08e491
klaus <1184224728@qq.com>
2020-12-17 11:11:31 +0800

    kafkaSource

---
+++ src/main/scala/com/klaus/source/kafkaSourceTest.scala
@@ -0,0 +1,31 @@
+package com.klaus.source
+
+import java.util.Properties
+
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011
+
+/**
+ * 测试flink消费kafka数据流
+ */
+
+object kafkaSourceTest {
+  def main(args: Array[String]): Unit = {
+    val environment = StreamExecutionEnvironment.getExecutionEnvironment
+
+
+    val properties = new Properties()
+    properties.setProperty("bootstrap.servers", "hadoop102:9092")
+    properties.setProperty("group.id", "consumer-group")
+    properties.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+    properties.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+    properties.setProperty("auto.offset.reset", "latest")
+    val kafkaStream = environment.addSource(new FlinkKafkaConsumer011[String]("sensor", new SimpleStringSchema(), properties))
+
+    kafkaStream.print("kafkaStream")
+    environment.execute()
+
+
+  }
+}
